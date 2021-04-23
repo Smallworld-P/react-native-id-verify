a5829cd7afa82fcf5e8413c9c8c3a06bf58dbf6e
jed <jedshim@163.com>
2021-04-23 11:41:05 +0800

    startVeritfy

--- android/src/main/java/com/rnverify/IdVerifyModule.java
+++ android/src/main/java/com/rnverify/IdVerifyModule.java
@@ -2,19 +2,25 @@
 
 package com.rnverify;
 
+import android.app.Activity;
+import android.telecom.Call;
+import android.widget.Toast;
+
+import com.alipay.mobile.android.verify.sdk.interfaces.ICallback;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
 
 import com.alipay.mobile.android.verify.sdk.ServiceFactory;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+
+import java.util.Map;
 
 public class IdVerifyModule extends ReactContextBaseJavaModule {
 
     private final ReactApplicationContext reactContext;
-    private String certifyId;
-    private String bizCode;
-    private boolean waitForResult = false;
 
     public IdVerifyModule(ReactApplicationContext reactContext) {
         super(reactContext);
@@ -28,13 +34,27 @@ public class IdVerifyModule extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
-    public void sampleMethod(String stringArgument, int numberArgument, Callback callback) {
-        // TODO: Implement some actually useful functionality
-        callback.invoke("Received numberArgument: " + numberArgument + " stringArgument: " + stringArgument);
+    private void getBizCode(Callback callback) {
+        callback.invoke(ServiceFactory.build().getBizCode(reactContext));
     }
 
-    private void getBizCode() {
-//        ServiceFactory.build().getBizCode(MainActivity.this)
+
+    @ReactMethod
+    public void startVerify(String bizCode, String certifyId, String url, Callback callback) {
+        // 封装认证数据
+        JSONObject requestInfo = new JSONObject();
+        requestInfo.put("url", url);
+        requestInfo.put("certifyId", certifyId);
+        requestInfo.put("bizCode", bizCode);
+
+        final Callback call = callback;
+        // 发起认证
+        ServiceFactory.build().startService(reactContext.getCurrentActivity(), requestInfo, new ICallback() {
+            @Override
+            public void onResponse(Map<String, String> response) {
+                call.invoke(response);
+            }
+        });
     }
 
 }
